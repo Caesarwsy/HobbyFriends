f31afda26c86d5edc2ca4571f93d6d9120208cc7
Caesarwsy <514698164@qq.com>
2015-09-25 14:19:52 +0800

    完成了基本框架
    完成了基本框架

---
+++ HobbyFriends/HobbyFriends/ViewControllers/LoginViewController/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  HobbyFriends
+//
+//  Created by Caesar on 15/9/25.
+//  Copyright © 2015年 Caesar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ HobbyFriends/HobbyFriends/ViewControllers/MainViewController/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  HobbyFriends
+//
+//  Created by Caesar on 15/9/25.
+//  Copyright © 2015年 Caesar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
